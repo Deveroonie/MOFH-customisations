4ab4452139be4913ad1e78799c34680519b8c413
Deveroonie <74148830+Deveroonie@users.noreply.github.com>
2022-07-21 19:25:33 +0100

    fix errors

--- hider.js
+++ hider.js
@@ -25,42 +25,42 @@ $(document).ready(function(){
 });*/
 
 
-const element = document.getElementById("item_attracta_seotips"); //hide SEOTOOLS - remove to unhide
-element.remove();
-const element = document.getElementById("icon-attracta_seotips"); // ^^^
-element.remove();
-const element = document.getElementById("icon-cloudflare"); // hide CLOUDFLARE - remove to unhideelement.remove();
-element.remove();
-const element = document.getElementById("item_cloudflare"); // ^^^
-element.remove();
-const element = document.getElementById("icon-sitereptile"); // hide sitebuilder - remove to unhide (although it was shut down)
-element.remove();
-const element = document.getElementById("item_sitereptile"); // ^^^
-element.remove();
-const element = document.getElementById("item_accounts"); // hide emailaccounts - remove to unhide (although it is paid)
-element.remove();
-const element = document.getElementById("icon-accounts"); // ^^^
-element.remove();
-const element = document.getElementById("item_forwarders"); // ^^^
-element.remove();
-const element = document.getElementById("icon-forwarders"); // ^^^
-element.remove();
-const element = document.getElementById("item_email_filters"); // ^^^
-element.remove();
-const element = document.getElementById("icon-email_filters"); // ^^^
-element.remove();
-const element = document.getElementById("item_postgresql_databases"); // hide postgresql databases - remove to unhide (although it is paid)
-element.remove();
-const element = document.getElementById("icon-postgresql_databases"); // ^^^ 
-element.remove();
-const element = document.getElementById("icon-remote_mysql"); // hide remotemysql - remove to unhide (it is paid, not applicable in free hosting)
-element.remove();
-const element = document.getElementById("item_remote_mysql"); // ^^^
-element.remove();
-const element = document.getElementById("imgPoweredByCpanel"); // hide poweredbycpanel - remove to unhide
-element.remove();
+const element1 = document.getElementById("item_attracta_seotips"); //hide SEOTOOLS - remove to unhide
+element1.remove();
+const element2 = document.getElementById("icon-attracta_seotips"); // ^^^
+element2.remove();
+const element3 = document.getElementById("icon-cloudflare"); // hide CLOUDFLARE - remove to unhideelement.remove();
+element3.remove();
+const element4 = document.getElementById("item_cloudflare"); // ^^^
+element4.remove();
+const element5 = document.getElementById("icon-sitereptile"); // hide sitebuilder - remove to unhide (although it was shut down)
+element5.remove();
+const element6 = document.getElementById("item_sitereptile"); // ^^^
+element6.remove();
+const element7 = document.getElementById("item_accounts"); // hide emailaccounts - remove to unhide (although it is paid)
+element7.remove();
+const element8 = document.getElementById("icon-accounts"); // ^^^
+element8.remove();
+const element9 = document.getElementById("item_forwarders"); // ^^^
+element9.remove();
+const element10 = document.getElementById("icon-forwarders"); // ^^^
+element10.remove();
+const element11 = document.getElementById("item_email_filters"); // ^^^
+element11.remove();
+const element12 = document.getElementById("icon-email_filters"); // ^^^
+element12.remove();
+const element13 = document.getElementById("item_postgresql_databases"); // hide postgresql databases - remove to unhide (although it is paid)
+element13.remove();
+const element14 = document.getElementById("icon-postgresql_databases"); // ^^^ 
+element15.remove();
+const element16 = document.getElementById("icon-remote_mysql"); // hide remotemysql - remove to unhide (it is paid, not applicable in free hosting)
+element16.remove();
+const element17 = document.getElementById("item_remote_mysql"); // ^^^
+element17.remove();
+const element18 = document.getElementById("imgPoweredByCpanel"); // hide poweredbycpanel - remove to unhide
+element18.remove();
 
 
 
-const element = document.getElementById("sidebar").style.display = "none";; //Remove Sidebar
-const element = document.getElementById("btnSideBarToggle").style.display = "none";; //Remove Sidebar Button on Mobile
+const sidebar = document.getElementById("sidebar").style.display = "none"; //Remove Sidebar
+const mblesidebar = document.getElementById("btnSideBarToggle").style.display = "none"; //Remove Sidebar Button on Mobile
